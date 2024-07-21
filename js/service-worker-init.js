91d61cba4da7d7bc572e431f2230a37477a18da8
Tim Retout <tim@retout.co.uk>
2024-07-21 20:21:07 +0100

    Support site.baseurl for hosting on a GitHub project page

--- js/service-worker-init.js
+++ js/service-worker-init.js
@@ -1,5 +1,7 @@
+---
+---
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('{{ site.baseurl }}/service-worker.js')
         .then(function(reg){
         }).catch(function(err) {
         console.log("No it didn't. This happened:", err)
